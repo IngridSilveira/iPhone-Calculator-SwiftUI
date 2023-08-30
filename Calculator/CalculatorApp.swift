b8fad1fcc8d36d2a2922358d106a2bbc37650ecd
Ingrid Silveira <ingridgsilveira@gmail.com>
2023-08-30 20:10:57 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by userext on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
